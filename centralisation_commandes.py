9a41495c45c957ea9af307f847741dbd2fba0220
Gé <zoso85@hotmail.fr>
2026-04-24 11:16:45 +0200

    Liste d'article et gestion erreur articles manquants OK

--- centralisation_commandes.py
+++ centralisation_commandes.py
@@ -1,16 +1,24 @@
 from openpyxl import load_workbook
-
 from dataclasses import dataclass
+from typing import List
 
 @dataclass
 class Article:
     name: str
     quantity: int
+
+@dataclass # classe des articles non trouvés
+class Error:
+    article: Article
+    client: str
     
 LOWEST_CLIENT_ROW = 4
 HIGHEST_CLIENT_ROW = 66
 
-def add_article(sheet, client, article: Article):
+CENTRAL_WORKBOOK = "Commandes.xlsx"
+OUTPUT_WORKBOOK = "Commandes2.xlsx"
+
+def add_article(sheet, client, article: Article, errors: List[Error]):
     for row in range(LOWEST_CLIENT_ROW, HIGHEST_CLIENT_ROW):
 
         order_name = sheet.cell(row=row, column=1).value
@@ -20,12 +28,34 @@ def add_article(sheet, client, article: Article):
             if order_quantity is None:
                 order_quantity = 0
             
-            #print(order_quantity)
-            #print(article.quantity)
             sheet.cell(row=row, column=11).value = order_quantity + article.quantity
             
             return  # on sort dès qu'on a trouvé
-        
+    print_missing_article(article)
+    add_missing_article(client, article, errors)
+    
+
+def print_missing_article(missing_article: Article):
+    print("!!! ATTENTION : l'article " + missing_article.name + " n'a pas été trouvé dans " + CENTRAL_WORKBOOK + " !!!")
+    print("Une cellule a peut être été modifiée.")
+    input("Appuyez sur Entrée...")
+
+def add_missing_article(client, article: Article, errors: List[Error]):
+    errors.append(Error(article=article, client=client))
+
+def print_errors(errors: List[Error]):
+    if errors:
+        print("!!! CERTAINS ARTICLES N'ONT PAS PU ETRE AJOUTES !!!")
+        for error in errors:
+            msg = (
+                f"{error.article.quantity} {error.article.name} pour le client {error.client} "
+                f"n'ont pas pu être ajoutés à {CENTRAL_WORKBOOK} "
+                f"car ils n'ont pas été trouvés."
+            )
+            print(msg)
+    if not errors:
+        print("Aucune erreur.")
+
 def read_articles(sheet):
     
     articles = []
@@ -46,32 +76,32 @@ def read_articles(sheet):
 
             current_article = Article(name, quantity_int)
             articles.append(current_article)
-            #print(current_article)
+            
 
     return articles
             
         
 def main():
     
-    # Charger le fichier
+    # Charger les fichiers
     client_workbook = load_workbook("Gamm vert.xlsx")
     client_sheet = client_workbook.active
 
-    orders_workbook = load_workbook("Commandes.xlsx")
+    orders_workbook = load_workbook(CENTRAL_WORKBOOK)
     orders_sheet = orders_workbook.active
 
-
-    
-            #add_article( orders_sheet, "Gamm vert", current_article)
-            
+    errors = []
     articles = read_articles(client_sheet)
+
 
     for article in articles:
         print(article)
-        add_article(orders_sheet, "Gamm vert", article)
+        add_article(orders_sheet, "Gamm vert", article, errors)
     
-    orders_workbook.save("Commandes2.xlsx")
-            
+    orders_workbook.save(OUTPUT_WORKBOOK)
+
+
+    print_errors(errors)
     print("Done")
 
 if __name__ == "__main__":
