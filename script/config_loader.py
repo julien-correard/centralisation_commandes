c156b5da07d97918d9026d68568b10263142a44d
Gé <zoso85@hotmail.fr>
2026-04-28 11:50:51 +0200

    Ajout de génération de config.ini par défaut

--- script/config_loader.py
+++ script/config_loader.py
@@ -38,9 +38,77 @@ def load_config(root_path: Path):
     path = root_path / "script" / "config.ini"
 
     if not path.exists():
-        raise FileNotFoundError(f"Fichier de config introuvable : {path}")
+        print (f"Fichier de config introuvable : {path}")
+        choice = input("\nRecréer un config.ini par défaut ? (o/n) : ")
+
+        if choice.lower() == "o":
+            create_default_config(path)
+        else:
+            raise FileNotFoundError(f"Fichier de config introuvable : {path}")
 
     parser = configparser.ConfigParser()
     parser.read(str(path))
 
-    return Config(parser, root_path)
+    try:
+        return Config(parser, root_path)
+
+    except Exception as e:
+        print("\n!!! LE FICHIER CONFIG.INI EST INVALIDE !!!")
+        print(e)
+
+        choice = input("\nRecréer un config.ini par défaut ? (o/n) : ")
+
+        if choice.lower() == "o":
+            backup = path.with_suffix(".broken.ini")
+            path.rename(backup)
+
+            create_default_config(path)
+            parser.read(path)
+
+            return Config(parser, root_path)
+
+        raise
+
+def create_default_config(path: Path):
+    content = """[CLIENT]
+; Première ligne utile du tableau client (premier article)
+LOWEST_CLIENT_ROW = 4
+
+; Dernière ligne utile des tableaux clients
+HIGHEST_CLIENT_ROW = 300
+
+
+[CENTRAL]
+; Première ligne utile du tableau central
+LOWEST_CENTRAL_ROW = 2
+
+; Dernière ligne utile du tableau central
+HIGHEST_CENTRAL_ROW = 300
+
+
+[LAYOUT]
+; Position du nom du client dans son fichier
+CLIENT_NAME_ROW = 1
+CLIENT_NAME_COLUMN = 3
+
+; Ligne et première colonne des clients dans le tableau central
+CLIENT_ROW = 1
+FIRST_CLIENT_COLUMN = 3
+
+
+[FILES]
+; Fichier modèle pour le tableau central
+CENTRAL_WORKBOOK = Commandes.xlsx
+
+; Fichier où sera enregistrée la sortie du programme
+OUTPUT_WORKBOOK = Commandes2.xlsx
+
+; Repertoire contenant les tableaux clients
+CLIENT_PATH = clients
+
+FICHIER_CORRESPONDANCE_CLIENTS = script/Correspondances_clients.csv
+
+"""
+
+    path.write_text(content, encoding="utf-8")
+    print(f"fichier {path} enregistré.")
